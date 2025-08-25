b7f69a7ee45a5cd62ba13c28a0f7d477349f581c
Jeremy Maloney <jeremyjmaloney@gmail.com>
2025-08-24 23:28:38 -0400

    Minor style updates

--- src/components/HeroCard/index.js
+++ src/components/HeroCard/index.js
@@ -1,7 +1,7 @@
 export const HeroCard = ({isRow = false, image, imageWidth="w-40", primaryText = "", secondaryText="", hasCta = false, ctaText="", href="#", id="", bgColor= "", margin="0", padding="0" }) => {
   return(
     <div id={id} className={`${bgColor} flex ${isRow ? "flex-row" : "flex-col"} items-center h-fit ${margin} ${padding}`}>
-      <img src={image} className={`rounded-2xl ${imageWidth} grayscale hover:blur-sm`} alt='profile headshot' />
+      <img src={image} className={`rounded-2xl ${imageWidth} grayscale hover:-skew-y-6`} alt='profile headshot' />
       {primaryText && (
         <p className="text-5xl font-semibold tracking-tight text-balance text-stone-600 sm:text-7xl my-10">
           {primaryText}
@@ -13,7 +13,7 @@ export const HeroCard = ({isRow = false, image, imageWidth="w-40", primaryText =
         </p>
       )}
       {hasCta && (
-        <a href={href} className="rounded-md bg-cyan-400 px-3.5 py-2.5 text-sm font-semibold text-stone-600 shadow-xs hover:bg-yellow-300 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-300">
+        <a href={href} className="rounded-md bg-cyan-400 px-3.5 py-2.5 text-sm font-semibold text-stone-600 shadow-xs hover:bg-yellow-300 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-300 duration-200 ease-in-out hover:-translate-y-1 hover:scale-110">
         {ctaText}
       </a>
       )}
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,7 +2,7 @@ export const Nav = () =>{
   return (
     <nav className="fixed top-0 w-full z-40 bg-slate-300 border-b border-black shadow-lg">
       <div className="mx-auto px-4">
-        <div className="flex justify-between items-center h-14">
+        <div className="flex justify-around items-center h-14">
           <a href="#home" className="text-xl font-bold">JEREMYJ<span className="text-cyan-400">MALONEY</span></a>
         </div>
       </div>
